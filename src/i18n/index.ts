3830587afb28c0ae3367dacdfe6b2b8e0b85b135
FT-FumihikoUnno <fumihiko.unno@futu-re.co.jp>
2026-01-06 06:37:42 +0000

    feat(i18n): add English locale (en.json) and register in i18n init

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,11 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import ja from './locales/ja.json'
+import en from './locales/en.json'
 
 i18n.use(initReactI18next).init({
   resources: {
+    en: { translation: en },
     ja: { translation: ja }
   },
   lng: 'ja',
